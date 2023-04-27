528bdbc56ad55d2b21d4962110476690d5bb76e3
zakariafethi <130164128+zakariafethi@users.noreply.github.com>
2023-04-27 16:33:36 +0200

    Add files via upload

---
+++ Metro.py
@@ -0,0 +1,10 @@
+class Metro(Vehicule):
+    def __init__(self, marque, matricule, couleur, nombreRoues, besoinChauffeur, vitesse, ligne):
+        super().__init__(marque, matricule, couleur, nombreRoues, besoinChauffeur, vitesse)
+        self._ligne = ligne
+
+    # Getter and setter for ligne
+    def get_ligne(self):
+        return self._ligne
+    def set_ligne(self, ligne):
+        self._ligne = ligne
